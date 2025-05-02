30299c9803784a54561570939a0cba9a443db696
chera-mihiretu <cheramihiretucm@gmail.com>
2025-05-03 00:07:22 +0300

    setting up the backend and starting auth

---
+++ backend/infrastracture/post_db.go
@@ -0,0 +1,27 @@
+package infrastracture
+
+import (
+	"database/sql"
+	"time"
+)
+
+func NewPostDB() (*sql.DB, error) {
+
+	connectionString := "postgresql://chera_mihiretu:1234556@localhost:5432/ethiopia_market_of_digitals?sslmode=disable"
+
+	db, err := sql.Open("postgres", connectionString)
+	if err != nil {
+		return nil, err
+	}
+
+	// Configure connection pool
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(25)
+	db.SetConnMaxLifetime(5 * time.Minute)
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,21 @@
-package delivery
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/chera-mihiretu/Ethiopian-Market-of-Digital/infrastracture"
+	_ "github.com/lib/pq"
+)
 
 func main() {
+	db, err := infrastracture.NewPostDB()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Database is connected Successfully")
+
+	defer db.Close()
 
 }
